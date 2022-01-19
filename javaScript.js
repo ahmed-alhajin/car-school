1517234252c8310be315e87a4b75727456be3424
ahmed alhajin <ahmed0599190439@gmail.com>
2022-01-19 11:50:47 +0200

    Add a website design to learn to drive

---
+++ javaScript.js
@@ -0,0 +1,15 @@
+/* global $, JQuery, alert*/
+
+
+
+$(".form_datetime").datetimepicker({
+    format: "dd MM yyyy - hh:ii",
+    linkField: "mirror_field",
+    linkFormat: "yyyy-mm-dd hh:ii"
+});
+
+
+
+
+
+
